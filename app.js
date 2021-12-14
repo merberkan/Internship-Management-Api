c59733823a47f2c4a5eaf59f804ef15b965bf533
merberkan <69236362+merberkan@users.noreply.github.com>
2021-12-15 00:43:34 +0300

    first commit
    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const path = require('path');
+// const mysql = require("mysql");
+const dotenv = require("dotenv");
+const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+
+// git check
+
+
+dotenv.config({path : './.env'})
+const app = express();
+
+//parse url-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({extended : false}));
+
+//parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+app.use(cookieParser());
+
+// app.use(session({ 
+//     secret: 'Özel-Anahtar',
+//     resave: false,
+//     saveUninitialized: true,
+// }))
+
+// db.connect((error) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log("MYSQL Connected...")
+//     }
+// })
+
+//define routes
+app.get("/", (req, res) => {
+    console.log("catched")
+});
+// app.use('/', require('./routes/pages'));
+// app.use('/auth' ,require('./routes/auth'));
+// app.use('/adminPanel',require('./routes/adminPanel'));
+// app.use('/ownerPanel',require('./routes/ownerPanel'));
+// app.use('/filterEvent',require('./routes/filterEvent'));
+// app.use((req,res) => {
+//     if(req.statusCode == null || req.statusCode == 404){
+//         res.redirect('/notFound');
+//     }
+// });
+
+
+app.listen(3000, () => {
+    console.log("server started at port 3000 ");
+});

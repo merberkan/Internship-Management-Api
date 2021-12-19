1318ceb61f6f26da80232ea80182120750a7b01a
Mert Berkan Akdeniz <mertberkanakdeniz@gmail.com>
2021-12-19 03:06:21 +0300

    login done

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const dotenv = require("dotenv");
 const initRoutes = require("./src/routes/Routes");
 
+
 global.__basedir = __dirname + "/..";
 dotenv.config({path : './.env'})
 
 app.use(express.urlencoded({ extended: true }));
+
 initRoutes(app);
-app.use(express.json());
 
 
 
---
+++ src/controllers/user/login.js
@@ -0,0 +1,48 @@
+const{
+    sign,
+    decode,
+    verify
+} = require('jsonwebtoken');
+const connectToDatabase = require("../../lib/database");
+
+const handler = async (req,res) => {
+    const { User, UserRole } = await connectToDatabase();
+    const model = req.body;
+    console.log(model);
+    const user = await User.findOne({ where: { Email: model.email, IsDeleted: false } });
+    if(user == null){
+        res.status(400).send({
+            message: "User_NotFound",
+          });
+    } 
+    if(user.Password !== model.password){
+        res.status(400).send({
+            message: "User_NotFound",
+          });
+    } 
+
+    const userRole = await UserRole.findOne({
+        where: {UserId: user.Id},
+        attributes: ['RoleId']
+    });
+
+    const claims = {
+        usercode: user.Id,
+        fullName: user.Name + user.Surname,
+        email: user.Email,
+        role: userRole.RoleId
+    }
+    const token = sign(claims, 'shhhhh');
+    res.status(200).send({
+        message: "Login Success",
+        data: {
+            token,
+            claims
+        },
+      });
+
+}
+
+module.exports = {
+    handler
+};
